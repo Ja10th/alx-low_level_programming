20c1e6cfa536764a5aacad3f207e0ad94fafb118
Ja10th <oluwaleyejames@gmail.com>
2022-09-08 02:14:12 -0700

    hmm

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,11 +9,7 @@ include <stdio.h>
  */
 
 int main(void)
-
 {
-
-		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return(0);
 }
