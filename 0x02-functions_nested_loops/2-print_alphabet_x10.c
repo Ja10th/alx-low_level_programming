15df0bae485c8c7bd4783667db469318d6c02ea1
Ja10th <oluwaleyejames@gmail.com>
2022-09-13 00:00:43 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - print 10 times alphabet in lower case
+ * /
+void print_alphabet_x10(void){
+	char i, l;
+	for (l=0; l<= 9; l++){
+		for (i='a'; i<= 'z' i++){
+			_putchar(i);
+	
+			}
+			_putchar('\n');
+	}
+
+}
