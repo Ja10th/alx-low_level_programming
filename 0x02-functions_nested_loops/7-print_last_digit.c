d8a9586249680ad97acb6b2ad662c8c67c537e5e
James Oluwaleye <100203073+Ja10th@users.noreply.github.com>
2022-09-13 08:42:30 +0100

    Printing last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+/**
+ * print_last_digit - checks number
+ */
+int print_last_digit(int j)
+{
+	int i;
+
+	i = j % 10;
+
+	if (i < 0)
+	{
+		i *= -1;
+	}
+	_putchar(i + '0');
+	return (i);
+}
