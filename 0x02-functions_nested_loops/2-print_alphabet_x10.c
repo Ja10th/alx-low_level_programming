67b0710b84dc283e89300137d3d4dc621920392c
James Oluwaleye <100203073+Ja10th@users.noreply.github.com>
2022-09-13 08:04:04 +0100

    Print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,14 @@
 void print_alphabet_x10(void)
 {
 	char i, l;
-	for (l=0; l<= 9; l++){
-		for (i='a'; i<= 'z' i++){
-			_putchar(i);
 	
-			}
-			_putchar('\n');
+	for (l=0; l<= 9; l++)
+	{
+	       for (i='a'; i<= 'z' i++){
+		  _putchar(i);
+	
+		}
+	  _putchar('\n');
 	}
 
 }
