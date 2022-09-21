523d313265094b63b1ac05be6d97c19c86ae98a0
James Oluwaleye <100203073+Ja10th@users.noreply.github.com>
2022-09-21 08:29:57 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,21 @@
-iiii
+#include "main.h"
+
+/**
+ * string_toupper - string
+ * @s: string
+ * Return: return s
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	for (; s[i]; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
