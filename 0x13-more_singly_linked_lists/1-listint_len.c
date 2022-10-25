3b332a05cb5f8cad4a17b27959d54b440ab1b10d
James Oluwaleye <100203073+Ja10th@users.noreply.github.com>
2022-10-25 15:31:08 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,16 @@
-hshhshsh
+#include "lists.h"
+
+/**
+ * listint_len - Function return len
+ * @h: pointer
+ *
+ * Return: size_t leng of the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+		h = h->next, i++;
+	return (i);
+}
